d216e8526494d040c786da90f09e198b9f9adb25
Shawn Wall <shawnkwall@gmail.com>
2013-07-09 08:25:36 -0400

    Initial Commit

---
+++ SSE Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SSE Test
+//
+//  Created by Shawn Wall on 7/9/13.
+//  Copyright (c) 2013 TwoTap Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
