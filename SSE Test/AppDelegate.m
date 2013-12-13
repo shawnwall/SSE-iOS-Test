6cf7eb8ec0936956664edb27901c173c9ef3f5fb
Shawn Wall <shawnkwall@gmail.com>
2013-12-13 09:42:36 -0500

    fixed ignore

--- SSE Test/AppDelegate.m
+++ SSE Test/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 #import "EventSourceManager.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
